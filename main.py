d6662b549d8e20bde9d24da005b3ba24cb267cba
Francesco Maresca <97321140+programmerhelloworld@users.noreply.github.com>
2022-05-12 21:07:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import random
 
+
+abj = ["strong", "dark", "magic", "cool"]
+words = ["gamer", "programmer", "tryharder", "mitico"]
+
+randword = random.randint(0, 4)
+randabj = random.randint(0,4)
+
+username = abj[randabj] + "-" + words[randword] + str(random.randint(0, 23))
+
+print(username)
