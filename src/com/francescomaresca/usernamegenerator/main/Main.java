25d743502dbb0d3495b1b6e01b6507a4a9430f28
Francesco Maresca <97321140+programmerhelloworld@users.noreply.github.com>
2023-01-09 18:24:00 +0100

    Update Main.java

--- src/com/francescomaresca/usernamegenerator/main/Main.java
+++ src/com/francescomaresca/usernamegenerator/main/Main.java
@@ -1,9 +1,31 @@
+/*
+Francesco Maresca's Username Generator.
+Copyright (C) 2023 Francesco Maresca (@programmerhelloworld). All rights reserved.
+*/
 package com.francescomaresca.usernamegenerator.main;
+import java.util.Random;
 
 public class Main {
-
+	static String username;
+	
+	static String[] abj = {"strong", "dark", "magic", "cool"};
+	
+	static String[] words = {"gamer", "programmer", "tryharder", "mitico"};
+	
+	
 	public static void main (String args[]) {
 		
 		
+	}
+		
+		public static void genUsername() {
+			GUI window = new GUI();
+			Random randomNum = new Random();
+			int randomabj = randomNum.nextInt(abj.length);
+			int randomword = randomNum.nextInt(words.length);
+			window.username = abj[randomabj] + "-" + words[randomword] + randomNum.nextInt(100);
+	
+	 
+	 
 	}
 }
